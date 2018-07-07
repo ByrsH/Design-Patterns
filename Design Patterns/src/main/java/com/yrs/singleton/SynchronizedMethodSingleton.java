708568692f13f9e4c1b9bb8be449f32eb7cb76b5
ByrsH <bgxjsh@sina.com>
2018-07-07 18:26:07 +0800

    fix

--- Design Patterns/src/main/java/com/yrs/singleton/SynchronizedMethodSingleton.java
+++ Design Patterns/src/main/java/com/yrs/singleton/SynchronizedMethodSingleton.java
@@ -21,7 +21,8 @@ public class SynchronizedMethodSingleton {
 
     /**
      * 在方法上加锁，能够保证同一时间只有一个线程访问，能够保证线程安全，但它的一个致命的缺点是性能问题。当singleton不为
-     * null时，也需要等待其他线程释放锁后才能访问getSingleton方法。获取锁和释放锁都需要一定的开销，改进方案双重校验锁。
+     * null时，也需要等待其他线程释放锁后才能访问getSingleton方法。对该方法的访问将变成串行，而且获取锁和释放锁都需要
+     * 一定的开销，因此会出现性能问题。改进方案双重校验锁。
      * @return
      */
     public synchronized static SynchronizedMethodSingleton getSingleton() {
