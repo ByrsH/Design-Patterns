6e7864682712bb854c1c26078c60031c8bdd517b
ByrsH <bgxjsh@sina.com>
2018-07-18 09:49:05 +0800

    prototype deep copy

---
+++ Design Patterns/src/main/java/com/yrs/prototype/DeepCopyByStreamPrototype.java
@@ -0,0 +1,28 @@
+package com.yrs.prototype;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * @Author: yangrusheng
+ * @Description:
+ * @Date: Created in 9:43 2018/7/18
+ * @Modified By:
+ */
+public class DeepCopyByStreamPrototype implements Serializable {
+
+    private ArrayList<String> arrayList = new ArrayList<String>();
+
+    public DeepCopyByStreamPrototype deepCopy() throws IOException, ClassNotFoundException {
+        //将对象写入流中
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(this);
+
+        //将对象从流中取出来
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        return (DeepCopyByStreamPrototype) ois.readObject();
+    }
+
+}
