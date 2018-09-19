a83c10750df6b3dfec497ed53e75116e32eba5b9
ByrsH <bgxjsh@sina.com>
2018-09-19 23:59:19 +0800

    add object adapter

---
+++ Design Patterns/src/main/java/com/yrs/adapter/objectAdapter/Adaptee2.java
@@ -0,0 +1,10 @@
+package com.yrs.adapter.objectAdapter;
+
+/**
+ * @program: design-patterns
+ * @description: 被适配类
+ * @author: yrs
+ * @create: 2018-09-19 18:48
+ **/
+public class Adaptee2 {
+}
---
+++ Design Patterns/src/main/java/com/yrs/adapter/objectAdapter/ObjectAdapter.java
@@ -0,0 +1,10 @@
+package com.yrs.adapter.objectAdapter;
+
+/**
+ * @program: design-patterns
+ * @description: 对象适配器
+ * @author: yrs
+ * @create: 2018-09-19 18:50
+ **/
+public class ObjectAdapter {
+}
